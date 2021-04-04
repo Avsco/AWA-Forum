bc7cf6c1305f34f133ba5bcfe2e5d08a07a4ade0
Alan Benavides <benacho82@gmail.com>
2021-04-03 23:12:57 -0400

    add: root route and process.env

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,16 @@ import router from "./router";
 import morgan from "morgan";
 
 class App {
-  private port = 5000;
+  private port = process.env.PORT || 5000;
   private app: Express = express();
 
   constructor() {
     this.config();
+    this.rootRoute();
     this.initRoutes();
   }
 
-  public initServer = async () => {
+  async initServer() {
     try {
       this.app.listen(this.port, () =>
         console.log(`Listening on http://${"localhost"}:${this.port}/`)
@@ -21,18 +22,24 @@ class App {
     } catch (error) {
       console.error(error);
     }
-  };
+  }
 
-  private config = () => {
+  private rootRoute() {
+    this.app.get("/", (_, res) => {
+      res.send("Hello World!");
+    });
+  }
+
+  private config() {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
     if (process.env.NODE_ENV !== "production") this.app.use(morgan("dev"));
-  };
+  }
 
-  private initRoutes = () => {
+  private initRoutes() {
     router(this.app);
-  };
+  }
 }
 
 export default new App();
